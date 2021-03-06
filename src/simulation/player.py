89da8dc13e0c9da3f13e5b7f3a50513a7e4d58e5
Maddie Louis <mlouis2@lion.lmu.edu>
2021-03-06 11:17:15 -0800

    some more sim outlining stuff

--- src/simulation/player.py
+++ src/simulation/player.py
@@ -18,6 +18,16 @@ class Strategy(Enum):
     MILITARISTIC = 1
     RESEARCH = 2
     SURVIVOR = 3
+    size = 4
+
+# Should this enum be replaced by the building cost dict? Maybe
+class Building(Enum):
+    OUTPOST = 0
+    ROAD = 1
+    BARRICADE = 2
+    KNIGHT = 3
+    APOTHECARY = 4
+    size = 5
 
 class Player:
     def __init__(self, index, strategy):
@@ -30,7 +40,7 @@ class Player:
         self.num_knights = 0
         self.defense_points = 0
         
-        # VVV This probably should/will be changed to a Castle class at some point,
+        # This probably should/will be changed to a Castle class at some point,
         # since we also need to know if the castle is infected or not.
         self.castle_horde_distance = [random.randint(1, MAX_HORDE_DISTANCE), random.randint(1, MAX_HORDE_DISTANCE)]
 
@@ -54,13 +64,31 @@ class Player:
         resource_name = Resource(resource).name
         for outpost in self.outposts:
             self.resources[resource_name] = self.resources[resource_name] + outpost[resource_name]
+
+    def can_build(self, building):
+        # TODO: create some sort of building cost dict
+        pass
+
+    def build(self, building):
+        # switch based off of the building enum and build it!
+        pass
     
     def handle_turn(self):
         if self.strategy == Strategy.GENERAL:
+            # Build order is random, attack somewhat randomly (Maddie)
+            build_order = [range(0, Building.size.value)]
+            random.shuffle(build_order)
+
+            for building in build_order:
+                if self.can_build(building):
+                    self.build(building)
             pass
         elif self.strategy == Strategy.MILITARISTIC:
+            # Build knights, outposts/roads, attacks whenever possible (Maddie)
             pass
         elif self.strategy == Strategy.RESEARCH:
+            # Build outposts, barricades, knights, attacks when horde distance is 1 (Merissa)
             pass
         elif self.strategy == Strategy.SURVIVOR:
+            # Build barricades, outposts, knights, only attacks to liberate castles (Merissa)
             pass
